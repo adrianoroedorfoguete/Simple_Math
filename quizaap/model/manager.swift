3d898c39806bc9e56b95d189f6e56bd166dc05ce
ICMMAC03-BEBA <icmmac03-beba@ICMMAC03-BEBAs-Mac-mini.local>
2022-11-14 18:34:45 -0400

    quizaap

--- quizaap/model/manager.swift
+++ quizaap/model/manager.swift
@@ -9,9 +9,8 @@ import Foundation
 
 class managerQuiz{
     var pergunta: [quiz] = [
+        
         quiz(label: "2+2 é igual a?", perguntas: ["4", "5", "3"], resposta:0),
-   
-    
         quiz(label: "2+3 é igual a?", perguntas: ["4", "5", "3"], resposta:1),
         quiz(label: "2+6 é igual a?", perguntas: ["4", "8", "3"], resposta:1),
         quiz(label: "3+3 é igual a?", perguntas: ["4", "5", "6"], resposta:2),
@@ -28,8 +27,6 @@ class managerQuiz{
         quiz(label: "9+1 é igual a?", perguntas: ["4", "10", "3"], resposta:1),
         quiz(label: "2+3 é igual a?", perguntas: ["4", "5", "3"], resposta:1),
         quiz(label: "1+2 é igual a?", perguntas: ["4", "5", "3"], resposta:2),
-        quiz(label: "6+7 é igual a?", perguntas: ["4", "13", "3"], resposta:1),
-        quiz(label: "4+2 é igual a?", perguntas: ["6", "5", "3"], resposta:0),
         quiz(label: "0+2 é igual a?", perguntas: ["4", "2", "3"], resposta:1),
         quiz(label: "3+9 é igual a?", perguntas: ["4", "5", "12"], resposta:2),
         quiz(label: "4+9 é igual a?", perguntas: ["13", "5", "3"], resposta:0),
@@ -44,13 +41,45 @@ class managerQuiz{
         quiz(label: "8+11 é igual a?", perguntas: ["4", "19", "3"], resposta:1),
         quiz(label: "2+11+5 é igual a?", perguntas: ["4", "5", "18"], resposta:2),
         
+        quiz(label: "2x7 é igual a?", perguntas: ["1", "14", "18"], resposta:1),
+        quiz(label: "2x4 é igual a?", perguntas: ["3", "9", "8"], resposta:2),
+        quiz(label: "3x9 é igual a?", perguntas: ["5", "27", "18"], resposta:1),
+        quiz(label: "5x9 é igual a?", perguntas: ["7", "9", "45"], resposta:2),
+        quiz(label: "7x5 é igual a?", perguntas: ["35", "5", "18"], resposta:0),
+        quiz(label: "6x8 é igual a?", perguntas: ["4", "48", "18"], resposta:1),
+        quiz(label: "3x2 é igual a?", perguntas: ["6", "5", "18"], resposta:0),
+        quiz(label: "4x4 é igual a?", perguntas: ["8", "16", "18"], resposta:1),
+        quiz(label: "5x3 é igual a?", perguntas: ["15", "5", "18"], resposta:0),
+        quiz(label: "7x7 é igual a?", perguntas: ["37", "5", "49"], resposta:2),
+        quiz(label: "8x8 é igual a?", perguntas: ["65", "64", "18"], resposta:1),
+        quiz(label: "2x9 é igual a?", perguntas: ["1", "5", "18"], resposta:2),
+        quiz(label: "3x1 é igual a?", perguntas: ["3", "56", "84"], resposta:0),
+        quiz(label: "1x9 é igual a?", perguntas: ["9", "2", "62"], resposta:0),
+        quiz(label: "4x8 é igual a?", perguntas: ["6", "33", "32"], resposta:2),
+        quiz(label: "9x3 é igual a?", perguntas: ["32", "27", "2"], resposta:1),
+        quiz(label: "8x4 é igual a?", perguntas: ["11", "1", "32"], resposta:2),
+        quiz(label: "9x9 é igual a?", perguntas: ["21", "81", "10"], resposta:1),
+        quiz(label: "5x6 é igual a?", perguntas: ["30", "7", "18"], resposta:0),
+        quiz(label: "5x2 é igual a?", perguntas: ["23", "5", "10"], resposta:2),
+        quiz(label: "5x5 é igual a?", perguntas: ["45", "25", "97"], resposta:1),
+        quiz(label: "7x8 é igual a?", perguntas: ["54", "59", "56"], resposta:2),
+        quiz(label: "9x2 é igual a?", perguntas: ["56", "18", "21"], resposta:1),
+        quiz(label: "9x4 é igual a?", perguntas: ["99", "0", "36"], resposta:2),
+        quiz(label: "3x6 é igual a?", perguntas: ["18", "54", "27"], resposta:0),
+        quiz(label: "9x7 é igual a?", perguntas: ["87", "63", "72"], resposta:1),
+        quiz(label: "2x2 é igual a?", perguntas: ["89", "4", "97"], resposta:1),
+        quiz(label: "6x6 é igual a?", perguntas: ["36", "16", "56"], resposta:0),
+        quiz(label: "7x4 é igual a?", perguntas: ["28", "75", "45"], resposta:0),
+        quiz(label: "8x1 é igual a?", perguntas: ["8", "35", "19"], resposta:0),
+        
+        
     ]
     
+    var minimo = 0
+    var maximo = 29
     
-    
-    var cont = -1
     func reloadQuiz() -> quiz{
-        cont = cont+1
+        var cont = Int.random(in: minimo...maximo)
         return pergunta[cont]
         
     }
